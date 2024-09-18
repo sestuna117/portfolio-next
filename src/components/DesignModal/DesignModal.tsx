18630880239cffab57cb28248fc5ecd8d2c45f19
Jason-Guo <jason.guo@ibm.com>
2024-09-18 11:28:30 -0400

    Fix text typo in design modal

--- src/components/DesignModal/DesignModal.tsx
+++ src/components/DesignModal/DesignModal.tsx
@@ -14,69 +14,69 @@ export const DesignModal = (props: DesignModalProps) => {
     isOpen && (
       <DelayedGlitchTransitionScreen text={"Design"} delay={2000}>
         <div
-            className={`fixed top-0 left-0 w-screen h-screen font-sans bg-black text-alto flex flex-col`}
-            id={"design-modal"}
+          className={`fixed top-0 left-0 w-screen h-screen font-sans bg-black text-alto flex flex-col`}
+          id={"design-modal"}
         >
           <svg
-              className={"absolute top-0 animate-slide-design-line-1"}
-              id={"design-line-1"}
-              height="3"
-              width="100%"
-              viewBox="0 0 100 50"
-              preserveAspectRatio="none"
+            className={"absolute top-0 animate-slide-design-line-1"}
+            id={"design-line-1"}
+            height="3"
+            width="100%"
+            viewBox="0 0 100 50"
+            preserveAspectRatio="none"
           >
             <line
-                x1="0"
-                y1="0"
-                x2="100"
-                y2="0"
-                stroke="white"
-                strokeWidth="100%"
-                vectorEffect="non-scaling-stroke"
+              x1="0"
+              y1="0"
+              x2="100"
+              y2="0"
+              stroke="white"
+              strokeWidth="100%"
+              vectorEffect="non-scaling-stroke"
             />
           </svg>
           <svg
-              className={"absolute bottom-0 animate-slide-design-line-2"}
-              id={"design-line-2"}
-              height="3"
-              width="100%"
-              viewBox="0 0 100 50"
-              preserveAspectRatio="none"
+            className={"absolute bottom-0 animate-slide-design-line-2"}
+            id={"design-line-2"}
+            height="3"
+            width="100%"
+            viewBox="0 0 100 50"
+            preserveAspectRatio="none"
           >
             <line
-                x1="0"
-                y1="0"
-                x2="100"
-                y2="0"
-                stroke="white"
-                strokeWidth="100%"
-                vectorEffect="non-scaling-stroke"
+              x1="0"
+              y1="0"
+              x2="100"
+              y2="0"
+              stroke="white"
+              strokeWidth="100%"
+              vectorEffect="non-scaling-stroke"
             />
           </svg>
           <div
-              className={`relative m-auto max-w-[1400px] w-fit h-fit md:w-[90%] before:animate-flicker-in
+            className={`relative m-auto max-w-[1400px] w-fit h-fit md:w-[90%] before:animate-flicker-in
                         before:h-full before:w-full before:content-empty before:absolute before:-z-10 
                         before:m-auto before:corner-border-white before:max-w-[1560px] before:block`}
-              id={"design-text"}
+            id={"design-text"}
           >
             <div
-                className={
-                  "animate-fade-in animation-delay-500 opacity-0 py-10 px-10 md:py-14 md:text-2xl lg:py-40 lg:text-4xl font-bold"
-                }
+              className={
+                "animate-fade-in animation-delay-500 opacity-0 py-10 px-10 md:py-14 md:text-2xl lg:py-40 lg:text-4xl font-bold"
+              }
             >
               The design for this portfolio was inspired by&nbsp;
               <a
-                  className={"text-heliotrope"}
-                  href={"https://pleasecallmechamp.com/"}
-                  target={"_blank"}
+                className={"text-heliotrope"}
+                href={"https://pleasecallmechamp.com/"}
+                target={"_blank"}
               >
                 Champ Panupong Techawongthawon
               </a>
               &nbsp;and&nbsp;
               <a
-                  className={"text-heliotrope"}
-                  href={"https://massive.work/"}
-                  target={"_blank"}
+                className={"text-heliotrope"}
+                href={"https://massive.work/"}
+                target={"_blank"}
               >
                 Massive Assembly&apos;s
               </a>
@@ -84,17 +84,16 @@ export const DesignModal = (props: DesignModalProps) => {
               Valorant by Riot Games of which I took inspiration from their use
               of bold fonts and animations transitions.&nbsp;
               <span className={"font-semibold"}>
-                Please do take a look at the works they&apos;ve done are deserving of
-                being checked out as they are all visually stunning and dynamic
+                Please take a look at the many other works they&apos;ve done as they
+                are all visually appealing and dynamic.
               </span>
-              .
             </div>
           </div>
           <button
-              className={`absolute top-5 right-5 md:top-2 md:right-2 lg:top-5 lg:right-top-5`}
-              onClick={close}
+            className={`absolute top-5 right-5 md:top-2 md:right-2 lg:top-5 lg:right-top-5`}
+            onClick={close}
           >
-            <IoMdClose className={"h-8 w-8 md:h-12 md:w-12"}/>
+            <IoMdClose className={"h-8 w-8 md:h-12 md:w-12"} />
           </button>
         </div>
       </DelayedGlitchTransitionScreen>
